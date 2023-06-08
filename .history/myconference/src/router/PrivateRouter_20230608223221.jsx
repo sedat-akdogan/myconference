4457684da2f7c7f22ad734cd0803b65d82457d24
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-06-08 22:33:15 +0100

    PrivateRouter is also completed

--- .history/myconference/src/router/PrivateRouter_20230608223221.jsx
+++ .history/myconference/src/router/PrivateRouter_20230608223221.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContextProvider";
 
 const PrivateRouter = () => {
     const { currentUser } = useContext(AuthContext);
