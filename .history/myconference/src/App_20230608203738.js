ab74fda2e18e21538d41635f05da5e30c7fdeeb1
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-06-08 20:40:38 +0100

    App.js, insex.css and index.js are completed

--- .history/myconference/src/App_20230608203738.js
+++ .history/myconference/src/App_20230608203738.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import AuthContextProvider from "./context/AuthContext";
+import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
 
 const App = () => {
